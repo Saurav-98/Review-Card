82033a52dd8218b28b950c1cb3a92b1315b3a391
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-07 01:57:33 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Review from './Review';
+import { GoMarkGithub } from 'react-icons/go';
+function App() {
+  return (
+    <h2>
+      reviews project setup <GoMarkGithub />
+    </h2>
+  );
+}
+
+export default App;
